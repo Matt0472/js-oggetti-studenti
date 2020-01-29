63e0c9137700478a7349b5ce58bee1d38c9b5465
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-29 17:35:51 +0100

    MISSION 2 COMPLETE BABY!

--- handlbars/javascript/script_2_bonus.js
+++ handlbars/javascript/script_2_bonus.js
@@ -2,12 +2,12 @@
 // In una sottocartella handlebars fate una pagina semplice con un input e un pulsante e inviate un messaggio a cui segue una risposta come avete fatto per boolzapp, usando però handlebars.
 $(document).ready(function() {
   $('.invia').click(function() {
-    var source = $("entry-template").html();
+    var source = $("#entry-template").html();
     var template = Handlebars.compile(source);
 
     var userName = $('#add-message').val();
     var context = {
-      'name': username
+      'name': userName
     };
     var html = template(context);
 
