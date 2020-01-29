05add837c611221dfaf259a5430b0ffb07f2a6b8
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-29 15:48:11 +0100

    creazione dell'oggetto, e stampa su schermo di tutte le sue proprietà

---
+++ handlbars/javascript/script_2_bonus.js
@@ -0,0 +1,2 @@
+// Esercizio 2
+// In una sottocartella handlebars fate una pagina semplice con un input e un pulsante e inviate un messaggio a cui segue una risposta come avete fatto per boolzapp, usando però handlebars.
--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,20 @@
+// Esercizio 1
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+// Usate prima i console.log se funziona tutto stampate a schermo con jQuery,
+// fatto questo provate ad usare Handlebars
+
+
+
 $(document).ready(function() {
-  
+  var student = {
+    'nome' : 'Filippo',
+    'cognome' : 'Inzaghi',
+    'eta' : 26
+  };
+
+  for (var key in student) {
+    console.log(student[key]);
+  }
 });
